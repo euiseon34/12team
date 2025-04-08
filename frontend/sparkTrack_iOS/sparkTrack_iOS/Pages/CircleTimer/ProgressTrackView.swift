96f2474a77b9864ccab11c54ea1a6b88035c7641
cinnamonfor6214 <seohyun2151@naver.com>
2025-04-08 16:42:26 +0900

    Feat: ProgressTrackView 생성

---
+++ frontend/sparkTrack_iOS/sparkTrack_iOS/Pages/CircleTimer/ProgressTrackView.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressTrackView.swift
+//  sparkTrack_iOS
+//
+//  Created by 박서현 on 4/8/25.
+//
+
+import SwiftUI
+
+struct ProgressTrackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressTrackView()
+}
