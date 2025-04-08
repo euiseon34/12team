6abb23ee234f82075a0240b84cff813a25f5ae24
cinnamonfor6214 <seohyun2151@naver.com>
2025-04-08 17:37:52 +0900

    Feat: CircleTimerView 위해 필요한 파일 생성

---
+++ frontend/sparkTrack_iOS/sparkTrack_iOS/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  sparkTrack_iOS
+//
+//  Created by 박서현 on 4/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
