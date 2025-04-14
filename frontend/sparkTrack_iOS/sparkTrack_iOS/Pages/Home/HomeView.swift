0ba5f34f435282e5ae1677190330bd97229e3f9b
cinnamonfor6214 <seohyun2151@naver.com>
2025-04-14 14:57:17 +0900

    Fix: 홈 뷰에 들어갈 뷰들 위치 확인

--- frontend/sparkTrack_iOS/sparkTrack_iOS/Pages/Home/HomeView.swift
+++ frontend/sparkTrack_iOS/sparkTrack_iOS/Pages/Home/HomeView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct HomeView: View {
-    var body: some View {
-        Text("홈 화면")
+  var body: some View {
+    
+    ScrollView() {
+      VStack {
+        Rectangle()
+          .frame(width: 380, height: 380)
+          .foregroundStyle(.gray)
+          .opacity(0.5)
+          .padding(.top, 80)
+        // 사분면 뷰 들어갈 예정
+        
+        Rectangle()
+          .frame(width: 380, height: 500)
+          .foregroundStyle(.gray)
+          .opacity(0.2)
+          .padding(.top, 30)
+        // 투두 뷰 들어갈 예정
+        
+      }
     }
+  }
 }
 
 #Preview {
