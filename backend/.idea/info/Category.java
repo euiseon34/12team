052967e267ec783b204065f997c64c3e22a0cbcd
euiseon34 <134220124+euiseon34@users.noreply.github.com>
2025-04-18 14:27:39 +0900

    Delete backend/.idea directory

--- backend/.idea/info/Category.java
+++
@@ -1,3 +0,0 @@
-public enum Category {
-    STUDY, EXERCISE, TEST, TASK, MEETING, TRAVEL, ETC
-}
--- backend/.idea/info/User.java
+++
@@ -1,6 +0,0 @@
-public class User {
-    String id;
-    String password, name, email, sex; // 비밀번호, 이름, 이메일, 성별
-    Integer focusSpan, restTime; // 집중 가능 시간(분), 휴식 시간(분)
-    String preferredReminderTime, chronoType; // 선호 알림 시간, 생체 리듬 유형
-}
