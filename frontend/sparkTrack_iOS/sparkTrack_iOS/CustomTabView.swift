064ef2859c8c433f210905a1b6cd1c239392491b
cinnamonfor6214 <seohyun2151@naver.com>
2025-04-25 13:24:16 +0900

    Fix: 탭바 불투명하게 변경

--- frontend/sparkTrack_iOS/sparkTrack_iOS/CustomTabView.swift
+++ frontend/sparkTrack_iOS/sparkTrack_iOS/CustomTabView.swift
@@ -19,64 +19,70 @@ struct CustomTabView: View {
   @Binding var selectedTab: Tab
   
   var body: some View {
-    VStack(spacing: 0){
+    ZStack {
       Rectangle()
-        .frame(height: 4)
+        .frame(height: 100)
+        .foregroundStyle(.white)
       
-      Spacer()
-      HStack {
-        Spacer()
-        Button(action: {selectedTab = .evaluation}){
-          ButtonView(
-            icon: Image(systemName: "pencil"),
-            title: "평가",
-            isSelected: selectedTab == .evaluation
-          )
-        }
+      VStack(spacing: 0){
+        Rectangle()
+          .frame(height: 4)
         
         Spacer()
-        Button(action: {selectedTab = .calendar}){
-          ButtonView(
-            icon: Image(systemName: "calendar"),
-            title: "캘린더",
-            isSelected: selectedTab == .calendar
-          )
+        HStack {
+          Spacer()
+          Button(action: {selectedTab = .evaluation}){
+            ButtonView(
+              icon: Image(systemName: "pencil"),
+              title: "평가",
+              isSelected: selectedTab == .evaluation
+            )
+          }
+          
+          Spacer()
+          Button(action: {selectedTab = .calendar}){
+            ButtonView(
+              icon: Image(systemName: "calendar"),
+              title: "캘린더",
+              isSelected: selectedTab == .calendar
+            )
+          }
+          
+          Spacer()
+          Button(action: {selectedTab = .home}){
+            ButtonView(
+              icon: Image(systemName: "house"),
+              title: "홈",
+              isSelected: selectedTab == .home
+            )
+          }
+          
+          Spacer()
+          Button(action: {selectedTab = .summary}){
+            ButtonView(
+              icon: Image(systemName: "trophy"),
+              title: "통계",
+              isSelected: selectedTab == .summary
+            )
+          }
+          
+          Spacer()
+          Button(action: {selectedTab = .user}){
+            ButtonView(
+              icon: Image(systemName: "person"),
+              title: "마이",
+              isSelected: selectedTab == .user
+            )
+          }
+          
+          Spacer()
         }
-        
-        Spacer()
-        Button(action: {selectedTab = .home}){
-          ButtonView(
-            icon: Image(systemName: "house"),
-            title: "홈",
-            isSelected: selectedTab == .home
-          )
-        }
-        
-        Spacer()
-        Button(action: {selectedTab = .summary}){
-          ButtonView(
-            icon: Image(systemName: "trophy"),
-            title: "통계",
-            isSelected: selectedTab == .summary
-          )
-        }
-        
-        Spacer()
-        Button(action: {selectedTab = .user}){
-          ButtonView(
-            icon: Image(systemName: "person"),
-            title: "마이",
-            isSelected: selectedTab == .user
-          )
-        }
-        
-        Spacer()
+        .frame(width: 400, height: 55)
+        .padding(.vertical, 15)
       }
-      .frame(width: 400, height: 55)
-      .padding(.vertical, 15)
+      .frame(height: 90)
+      .edgesIgnoringSafeArea(.bottom)
     }
-    .frame(height: 90)
-    .edgesIgnoringSafeArea(.bottom)
   }
 }
 
