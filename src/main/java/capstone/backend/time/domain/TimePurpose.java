16fd1ea7458fc490cdd57538eb2963efc30fb194
dlwldn30 <dlwldn30@naver.com>
2025-04-28 14:22:59 +0900

    파일 구조 변경

--- src/main/java/capstone/backend/time/domain/TimePurpose.java
+++
@@ -1,9 +0,0 @@
-package capstone.backend.time.domain;
-
-public enum TimePurpose {
-    FOCUSED_WORK,
-    LIGHT_TASKS,
-    ROUTINE_REVIEW,
-    RELAXATION,
-    FREE
-}
--- src/main/java/capstone/backend/time/repository/PreferredTimeRepository.java
+++
@@ -1,7 +0,0 @@
-package capstone.backend.time.repository;
-
-import capstone.backend.time.domain.PreferredTime;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PreferredTimeRepository extends JpaRepository<PreferredTime, Long> {
-}
