93787fbf28cf7c4e7dbf61c490fa702f699ac4cb
cinnamonfor6214 <seohyun2151@naver.com>
2025-06-12 13:37:38 +0900

    Chore: 주석 삭제

--- frontend/sparkTrack_iOS/sparkTrack_iOS/ScheduleBoard/ScheduleBoardView.swift
+++ frontend/sparkTrack_iOS/sparkTrack_iOS/ScheduleBoard/ScheduleBoardView.swift
@@ -5,13 +5,6 @@
 //  Created by 박서현 on 5/30/25.
 //
 
-//
-//  ScheduleBoardView.swift
-//  sparkTrack_iOS
-//
-//  Created by 박서현 on 5/30/25.
-//
-
 import SwiftUI
 
 struct ScheduleBoardView: View {
